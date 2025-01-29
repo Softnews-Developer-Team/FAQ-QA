795efb196e8707f9e51d11e9b8d8929b6b336fa0
Sávio Morais | Softnews <savio.morais@go.softnewss.com.br>
2025-01-29 14:58:24 -0300

    DOCS:Implementado respostas no arquivo 'faqs-exportacao.js'

--- pages/conversao-dados/faqs-conversao.js
+++ pages/conversao-dados/faqs-conversao.js
@@ -1,30 +1,30 @@
 export const faqs = [
     {
         pergunta: "Quais são as informações que a conversão de dados trás?",
+        resposta: "Atualmente a conversão de dados trás <b>somente</b> informações do módulo Folha (também chamado de Departamento Pessoal/DP por alguns sistemas).<br><br>Caso você queira ver exatamente quais dados são convertidos para um sistema concorrente expecifico, faça o seguinte procedimento:<ol><li>Vá no seguinte caminho do FTP: <code style='font-size: 1rem'>/unidades/Pub/Conversores/[Conversor Thomson Reuters]</code> e baixe o arquivo <b>Conversor-Thomson-Reuters.zip</b></li><li>Descompacte a pasta e acesse o arquivo: <b>Manual_Conversores_Thomson_Reuters.html</b></li><li>Vá na guia ''DADOS CONVERTIDOS'' &gt; ''Ctrl + F'' &gt; Pesquise o nome do sistema Concorrente &gt; Veja uma lista indicando em ''S'' quais são os itens que aquele sistema converte.</li></ol>"
+    },
+    {
+        pergunta: "Quando fez o processo de teste e não acha nenhuma inconsistências, é preciso marcar e/ou realizar o processo oficial?",
         resposta: "<p> &#x1F6A7; Em construção &#x1F6A7;</p>"
-      },
-      {
-          pergunta: "Quando fez o processo de teste e não acha nenhuma inconsistências, é preciso marcar e/ou realizar o processo oficial?",
-          resposta: "<p> &#x1F6A7; Em construção &#x1F6A7;</p>"
-      },
-      {
-          pergunta: "Qual a diferença do processo de conversão via eSocial para as demais conversões?",
-          resposta: "<p> &#x1F6A7; Em construção &#x1F6A7;</p>"
-      },
-      {
-          pergunta: "Centralização de filiais",
-          resposta: "<p> &#x1F6A7; Em construção &#x1F6A7;</p>"
-      },
-      {
-          pergunta: "Onde verificar o faseamento da empresa?",
-          resposta: "<p> &#x1F6A7; Em construção &#x1F6A7;</p>"
-      },
-      {
-          pergunta: "Conversão concluida, para quem enviar?",
-          resposta: "<p> &#x1F6A7; Em construção &#x1F6A7;</p>"
-      },
-      {
-          pergunta: "Como utilizar o Utilitário ETL para coleta de backups concorrentes?",
-          resposta: "<p> &#x1F6A7; Em construção &#x1F6A7;</p>"
-      }
+    },
+    {
+        pergunta: "Qual a diferença do processo de conversão via eSocial para as demais conversões?",
+        resposta: "<p> &#x1F6A7; Em construção &#x1F6A7;</p>"
+    },
+    {
+        pergunta: "Centralização de filiais",
+        resposta: "<p> &#x1F6A7; Em construção &#x1F6A7;</p>"
+    },
+    {
+        pergunta: "Onde verificar o faseamento da empresa?",
+        resposta: "<p> &#x1F6A7; Em construção &#x1F6A7;</p>"
+    },
+    {
+        pergunta: "Conversão concluida, para quem enviar?",
+        resposta: "<p> &#x1F6A7; Em construção &#x1F6A7;</p>"
+    },
+    {
+        pergunta: "Como utilizar o Utilitário ETL para coleta de backups concorrentes?",
+        resposta: "<p> &#x1F6A7; Em construção &#x1F6A7;</p>"
+    }
 ]
--- pages/exporta-importa/faqs-exporta.js
+++ pages/exporta-importa/faqs-exporta.js
@@ -1,10 +1,10 @@
 export const faqs = [
     {
       pergunta: "Na realização do processo oficial, qual é o banco que não deverá utilizar o sistema Domíno?",
-      resposta: "<p> &#x1F6A7; Em construção &#x1F6A7;</p>"
+      resposta: "Quando ocorre o processo oficial de Exporta/Importa, o cliente que <u>não poderá utilizar o sistema Domínio</u>, sempre será o <b>DESTINO</b>."
     },
     {
       pergunta: "Quando e como deve ser cadastrado a SSC de disponibilização de banco ORIGEM?",
-      resposta: "<p> &#x1F6A7; Em construção &#x1F6A7;</p>"
+      resposta: "A SSC de disponibilização do banco ORIGEM deve ser cadastrada sempre que há um processo de Exporta/Importa onde o <mark style='background-color: #FAAF00'>licenciado ORIGEM é diferente do licenciado DESTINO</mark>.<br><br>Sobre o cadastro da SSC, a mesma deva ser realizada do seguinte modo:<br><h2>Bancos Domínio Local:</h2><ul><li><b>Versão</b>: A mais recente disponivel</li><li><b>Classificação</b>: TÉCNICA</li><li><b>Sistema</b>: Exporta/Importa</li><li><b>Módulo</b>: Geral</li><li><b>Tópico</b>: Entre clientes</li><li><b>Subtópicos</b>:  Entre clientes</li><img src='../../images/exporta-importa/Quando e como deve ser cadastrado a SSC de disponibilização de banco ORIGEM/exemplo_cadastro_banco_local.png' alt='exemplo_cadastro_banco_local'></img></ul><hr><h2>Bancos Domínio Web:</h2><ul><li><b>Versão</b>: A mais recente disponivel</li><li><b>Classificação</b>: TÉCNICA</li><li><b>Sistema</b>: DOMINIO WEB</li><li><b>Módulo</b>: Geral</li><li><b>Tópico</b>: Backup - Disponibilizar Backup</li><li><b>Subtópicos</b>:  Backup - Disponibilizar Backup</li><img src='../../images/exporta-importa/Quando e como deve ser cadastrado a SSC de disponibilização de banco ORIGEM/exemplo_cadastro_banco_dw.png' alt='exemplo_cadastro_banco_dw'></img></ul>"
     }
 ];
